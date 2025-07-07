7558ce9974f3d9ae9ed38f7274db51e4cfc7a271
Uchiha-lai <128663807+Uchiha-lai@users.noreply.github.com>
2025-07-07 13:57:27 -0700

    Words Sort
    Created the file with all the words, and a function to initialize the word dictionary sorted by alphabet.

--- Wordle.py
+++ Wordle.py
@@ -1,1 +1,9 @@
-print ("Testing")
+wordDictionary = {"a":[],"b":[],"c":[],"d":[],"e":[],"f":[],"g":[],"h":[],"i":[],"j":[],"k":[],"l":[],"m":[],"n":[],"o":[],"p":[],"q":[],"r":[],"s":[],"t":[],"u":[],"v":[],"w":[],"x":[],"y":[],"z":[]}
+def initializeWords():
+    global wordDictionary
+    with open("words.txt", 'r') as words:
+        for i in words:
+            i = i.lower()
+            i = i.strip()
+            firstLetter = i[0]
+            wordDictionary[firstLetter].append(i)
