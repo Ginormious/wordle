e17a67a25e5bf9b40ab92ef85e186713df58111c
Uchiha-lai <128663807+Uchiha-lai@users.noreply.github.com>
2025-07-09 13:37:42 -0700

    continue playing option

--- Wordle.py
+++ Wordle.py
@@ -5,6 +5,7 @@ words = ['Genius', 'Magnificent', 'Impressive', 'Splendid', 'Great', 'Phew']
 wordDictionary = {"a":[],"b":[],"c":[],"d":[],"e":[],"f":[],"g":[],"h":[],"i":[],"j":[],"k":[],"l":[],"m":[],"n":[],"o":[],"p":[],"q":[],"r":[],"s":[],"t":[],"u":[],"v":[],"w":[],"x":[],"y":[],"z":[]}
 letter = []
 random_word = ""
+CYAN="\033[36m"
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
 RESET = "\033[0m"
@@ -27,7 +28,7 @@ def game():
     guessNum = 1
     guessWord = ""
     while guessNum < 7:
-        print("Enter your {} guess:".format(str(guessNum)), end = "")
+        print(f"{CYAN}Enter your "+"{} guess:".format(str(guessNum))+f"{RESET}", end = "")
         guessWord = input()
         if guessWord == random_word:
         
@@ -46,87 +47,15 @@ def game():
                     print(str(guessWord[i]), 'Is', 'Gray')
         guessNum += 1
     print("The word was", str(random_word))
+
+
+gameover = False
 initializeWords()
-getWord()
-game()
-"""
-first_try = input('Enter your first guess: ')
-
-
-second_try = input('Enter your second guess: ')
-if second_try == random_word:
-    print("Magnificent")
-    quit()
-if len(second_try) != 5:
-    print('Please enter a 5 character word')
-else:
-    for i in range(len(random_word)):
-        if second_try[i] == random_word[i]:
-            print(str(second_try[i]), 'Is', 'Green')
-        elif second_try[i] in random_word:
-            print(str(second_try[i]), 'Is', 'Yellow')
-        else:
-            print(str(second_try[i]), 'Is', 'Gray')
-
-third_try = input('Enter your third guess: ')
-if third_try == random_word:
-    print("Impressive")
-    quit()
-if len(third_try) != 5:
-    print('Please enter a 5 character word')
-else:
-    for i in range(len(random_word)):
-        if third_try[i] == random_word[i]:
-            print(str(third_try[i]), 'Is', 'Green')
-        elif third_try[i] in random_word:
-            print(str(third_try[i]), 'Is', 'Yellow')
-        else:
-            print(str(third_try[i]), 'Is', 'Gray')
-
-fourth_try = input('Enter your fourth guess: ')
-if fourth_try == random_word:
-    print("Splendid")
-    quit()
-if len(fourth_try) != 5:
-    print('Please enter a 5 character word')
-else:
-    for i in range(len(random_word)):
-        if fourth_try[i] == random_word[i]:
-            print(str(fourth_try[i]), 'Is', 'Green')
-        elif fourth_try[i] in random_word:
-            print(str(fourth_try[i]), 'Is', 'Yellow')
-        else:
-            print(str(fourth_try[i]), 'Is', 'Gray')
-
-fifth_try = input('Enter your fifth guess: ')
-if fifth_try == random_word:
-    print("Great")
-    quit()
-if len(fifth_try) != 5:
-    print('Please enter a 5 character word')
-else:
-    for i in range(len(random_word)):
-        if fifth_try[i] == random_word[i]:
-            print(str(fifth_try[i]), 'Is', 'Green')
-        elif fifth_try[i] in random_word:
-            print(str(fifth_try[i]), 'Is', 'Yellow')
-        else:
-            print(str(fifth_try[i]), 'Is', 'Gray')
-
-sixth_try = input('Enter your sixth guess: ')
-if sixth_try == random_word:
-    print("Phew")
-    quit()
-if len(sixth_try) != 5:
-    print('Please enter a 5 character word')
-else:
-    for i in range(len(random_word)):
-        if sixth_try[i] == random_word[i]:
-            print(str(sixth_try[i]), 'Is', 'Green')
-        elif sixth_try[i] in random_word:
-            print(str(sixth_try[i]), 'Is', 'Yellow')
-        else:
-            print(str(sixth_try[i]), 'Is', 'Gray')
-
-
-"""
+while not gameover:
+    getWord()
+    game()
+    over = input("Another Game?(y/n):")
+    if(over.lower()=="n"):
+        gameover = True
+        break
+    os.system("cls")
